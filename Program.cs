4583ec04ec92bb6efa54f5c3a4f0fb829991d94a
solaries <solarinolakunle@yahoo.com>
2021-01-17 14:07:41 +0100

    just created the console project that would test the utility class, no code of mine in it yet

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Health_Provider_Listing_Simplify
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
